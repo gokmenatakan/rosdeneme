bff003860bd8676f8921d35bf22e82667dbcf959
Gökmen Atakan Türkmen <47220767+gokmenatakan@users.noreply.github.com>
2019-08-26 16:10:04 +0300

    Add files via upload

--- ros_analog/ros_analog.ino
+++ ros_analog/ros_analog.ino
@@ -2,7 +2,7 @@
 #include <rosserial_arduino/Adc.h>
 ros::NodeHandle nh;
 rosserial_arduino::Adc adc_msg;
-ros::Publisher p("adc", &adc_msg);
+ros::Publisher p("adc",&adc_msg);
 const int analog_pin = A0;
 void setup()
 {
@@ -17,11 +17,13 @@ void loop()
 }
 float kalman (int pin_num)
 {
-  float input = analogRead(pin_num);
+  
+  float aValue = analogRead(pin_num);
+  float input = map(aValue, 102, 922, -640, 640);
   float kalman_old;
   float cov_old;
-  float kalman_new = kalman_old; // eski değer alınır
-  float cov_new = cov_old + 0.50; //yeni kovaryans değeri belirlenir. Q=0.50 alınmıştır
+  float kalman_new = kalman_old; //eski değer alınır
+  float cov_new = cov_old + 0.50;//yeni kovaryans değeri belirlenir. Q=0.50 alınmıştır
   float kalman_gain = cov_new / (cov_new + 0.9); //kalman kazancı hesaplanır. R=0.9 alınmıştır
   float kalman_calculated = kalman_new + (kalman_gain * (input - kalman_new)); //kalman değeri hesaplanır
   cov_new = (1 - kalman_gain) * cov_old; //yeni kovaryans değeri hesaplanır
